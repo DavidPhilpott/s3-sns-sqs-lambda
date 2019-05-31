c8763715c15eafd1de7de5779ab32d4c1e92ff0e
David Philpott <DavidJohnPhilpott@outlook.com>
2019-05-31 22:04:58 +0100

    updated backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "s3-sns-sqs-lambda-test-bucket"
-    key    = "terraform_state"
-    region = "us-east-1"
+    bucket = "djphilpott-terraform-states"
+    key    = "s3-sns-sqs-lambda/terraform_state"
+    region = "eu-west-1"
   }
 }
