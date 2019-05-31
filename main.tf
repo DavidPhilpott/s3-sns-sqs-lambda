0d1f634b19fdcafa60af9e35a32bad11f12f0a16
David Philpott <DavidJohnPhilpott@outlook.com>
2019-05-31 22:20:18 +0100

    Added delivery_policy to sns

--- main.tf
+++ main.tf
@@ -11,4 +11,23 @@ resource "aws_s3_bucket" "s3-sns-sqs-lambda-test-bucket" {
 
 resource "aws_sns_topic" "s3-sns-sqs-lambda-sns-topic" {
   name = "s3-sns-sqs-lambda-sns-topic"
+    delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultThrottlePolicy": {
+      "maxReceivesPerSecond": 1
+    }
+  }
+}
+EOF
 }
