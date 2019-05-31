57b713749742e7229528a073644d5d780d68c055
David Philpott <DavidJohnPhilpott@outlook.com>
2019-05-31 22:21:05 +0100

    Added tags to sns topic

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "s3-sns-sqs-lambda-test-bucket" {
   acl    = "private"
 
   tags = {
-  	Project = "s3-sns-sqs-lambda"
+  	Project     = "s3-sns-sqs-lambda"
     Name        = "s3-sns-sqs-lambda-test-bucket"
     Environment = "Dev"
   }
@@ -11,7 +11,12 @@ resource "aws_s3_bucket" "s3-sns-sqs-lambda-test-bucket" {
 
 resource "aws_sns_topic" "s3-sns-sqs-lambda-sns-topic" {
   name = "s3-sns-sqs-lambda-sns-topic"
-    delivery_policy = <<EOF
+
+  tags = {
+    Project     = "s3-sns-sqs-lambda"
+    Name        = "s3-sns-sqs-lambda-sns-topic"
+    Environment = "Dev"
+  delivery_policy = <<EOF
 {
   "http": {
     "defaultHealthyRetryPolicy": {
