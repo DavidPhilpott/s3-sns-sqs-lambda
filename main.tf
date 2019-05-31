226f14e5923398d2a256abe353eebda17a5a6444
David Philpott <DavidJohnPhilpott@outlook.com>
2019-05-31 22:16:56 +0100

    Added project tag

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_s3_bucket" "input_sns_sqs_bucket" {
   acl    = "private"
 
   tags = {
+  	Project = "s3-sns-sqs-lambda"
     Name        = "s3-sns-sqs-lambda-test-bucket"
     Environment = "Dev"
   }
