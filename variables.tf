d63d4762e363ea1b729c690a91b0451bf7d434aa
David Philpott <DavidJohnPhilpott@outlook.com>
2019-06-03 14:50:29 +0100

    Updated variable file

--- variables.tf
+++ variables.tf
@@ -1,6 +1,20 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {}
-variable "project-name" {}
-variable "s3-bucket-name" {}
-variable "sns-topic-name" {}
+variable "access_key" {
+	type = "string"
+}
+variable "secret_key" {
+	type = "string"
+}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "project-name" {
+	type = "string"
+}
+
+locals {
+  common_tags = "${map(
+        "project_name", "${var.project_name}"
+    )}"
+}
